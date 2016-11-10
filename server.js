b76bdca89c5f16ee2351d8c58dcfc8088ce12ecd
Ionut Colceriu <contact@ghinda.net>
2016-11-10 09:49:21 +0200

    health endpoints, and 200 for root.

--- server.js
+++ server.js
@@ -12,8 +12,18 @@ var linkService = require('./link-service')
 var api = require('./routes/api')(linkService)
 app.use('/api/', api)
 
+// health
+app.get('/health', function (req, res) {
+  res.send()
+})
+
 // shorturl resolver
 var resolve = require('./routes/resolve')(linkService)
 app.use('/', resolve)
+
+// root
+app.get('/', function (req, res) {
+  res.send()
+})
 
 module.exports = app.listen(config.port, config.ip)
